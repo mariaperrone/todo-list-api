af75188c687f324ced830f3a5767438db4649274
mariaperrone <perronemar148@gmail.com>
2021-09-23 00:44:12 +0000

    in progress

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
 	const [tarea, setTarea] = useState("");
@@ -6,27 +6,37 @@ const Home = () => {
 
 	const url = "https://assets.breatheco.de/apis/fake/todos/user/mariaperrone";
 
-	fetch(url, {
-		method: "GET",
-		headers: {
-			"Content-Type": "application/json"
-		}
-	})
-		.then(resp => {
-			return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+	const getTodo = () => {
+		fetch(url, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			}
 		})
-		.then(data => {
-			//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-			console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
-		})
-		.catch(error => {
-			//manejo de errores
-			console.log(error);
-		});
+			.then(resp => {
+				return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(data => {
+				//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+				setListaTareas(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+			})
+			.catch(error => {
+				//manejo de errores
+				alert(error);
+			});
+	};
+
+	useEffect(() => {
+		getTodo();
+	}, []);
 
 	const handleKeyPress = event => {
 		if (event.key == "Enter") {
-			setListaTareas([...listaTareas, tarea]);
+			let newtarea = {
+				label: tarea,
+				done: false
+			};
+			setListaTareas([...listaTareas, newtarea]);
 			setTarea("");
 		}
 	};
@@ -51,7 +61,7 @@ const Home = () => {
 				{listaTareas.map((item, index) => {
 					return (
 						<li key={index}>
-							{item}
+							{item.label}
 							<button
 								className="btn btn-light text-dark"
 								type="submit"
